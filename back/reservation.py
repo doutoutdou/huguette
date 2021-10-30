f3612eb481e22783ab9f857c8541be63ee61eca4
doudou <edouard.talma@gmail.com>
2021-10-30 13:24:41 +0200

    clean

--- back/reservation.py
+++ back/reservation.py
@@ -26,25 +26,6 @@ class ReservationSchema(Schema):
     day = fields.Str()
     booked = fields.Boolean()
 
-# Data to serve with our API
-RESERVATION = {
-    "day1": {
-        "id" : "1",
-        "day": "01/01/2021",
-        "booked": "false"
-    },
-    "day2": {
-        "id" : "2",
-        "day": "01/02/2021",
-        "booked": "false"
-    },
-    "day3": {
-        "id" : "3",
-        "day": "01/03/2021",
-        "booked": "true"
-    }
-}
-
 # Create a handler for our read (GET) people
 def read_all():
     """
@@ -54,19 +35,9 @@ def read_all():
     :return:        sorted list of people
     """
     # Create the list of people from our data
-    #return [RESERVATION[key] for key in sorted(RESERVATION.keys())]
     conn = get_db_connection()
     meta = MetaData()
 
-    class Reservation:
-        def __init__(self, id, day, booked):
-            self.id = id
-            self.day = day
-            self.booked = booked
-
-        def __repr__(self):
-            return "<Reservation(id={self.id!r})>".format(self=self)
-            
     reservations = Table(
         'reservation', meta, 
         Column('id', Integer, primary_key = True), 
@@ -79,35 +50,12 @@ def read_all():
         day = fields.Str()
         booked = fields.Bool()
 
-        @post_load
-        def make_user(self, data, **kwargs):
-            return Reservation(**data)
-
-    request = text("SELECT * from reservation where id = '1'")
+    request = reservations.select()
     result = conn.execute(request)
-
-#    cursor = conn.cursor()
-#    reservation = cursor.execute('SELECT * from reservation where ').fetchone()[1]
-    #rows = cursor.fetchall()
-
-    #print(result.fetchone())
-    #for row in result:
-    #    print(row)
 
     schema = ReservationSchema(many=True)
     jsonResult = schema.dump(result)
-    for row in jsonResult:
-        print(row)
-    #    swagger = row
 
-    #print(jsonResult)
-    # schema = ReservationSchema()
-    #result = schema.dump(reservation)
-    #print(reservation)
-    #conn.close
-
-    #results = [list(row) for row in result]
-    #result_dict = {'results': results}
     return jsonResult
 
 def read(id):
